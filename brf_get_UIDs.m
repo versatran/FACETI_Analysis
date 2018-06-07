70818cac70bca19b4838279753aab2b871e0db81
Brianna <brianna.florio@stonybrook.edu>
2018-06-07 11:37:55 -0400

    Created files for basket-picking(nothing in them yet)

---
+++ brf_get_UIDs.m
@@ -0,0 +1,12 @@
+
+% The purpose of this function is for the "Linecut" function.
+% This function will find the UID's that match with the corresponding
+% condition
+% This is the FIRST step to getting the values to be plotted with linecuts
+
+% Parameters should be entered as followed:
+% (UID's, condition)
+% Output the UID's found from the corresponding values
+function brf_get_UIDs(varargin)
+
+end
---
+++ brf_get_Values.m
@@ -0,0 +1,12 @@
+
+% The purpose of this function is for the "Linecut" function.
+% This function will find the values that match with the correspondig UID's
+% This is the SECOND and LAST step to getting the values to be plotted with linecuts
+
+% Parameters should be entered as followed:
+% (UID's, values to pick from)
+% Output the values found with the UID's
+
+function brf_get_Values(varargin)
+
+end
